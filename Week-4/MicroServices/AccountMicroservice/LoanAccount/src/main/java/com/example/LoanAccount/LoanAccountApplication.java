6fd766704000aea1460ef1c1c9a5aa4b47fcb89d
UmarFarooq7432 <farooqumar8286@gmail.com>
2026-07-22 11:34:20 +0530

    Added week 4

---
+++ Week-4/MicroServices/AccountMicroservice/LoanAccount/src/main/java/com/example/LoanAccount/LoanAccountApplication.java
@@ -0,0 +1,13 @@
+package com.example.LoanAccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanAccountApplication.class, args);
+	}
+
+}
---
+++ Week-4/MicroServices/AccountMicroservice/LoanAccount/src/main/java/com/example/LoanAccount/LoanController.java
@@ -0,0 +1,20 @@
+package com.example.LoanAccount;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/loans")
+public class LoanController {
+
+    @GetMapping("/{number}")
+    public Loan getLoan(@PathVariable String number) {
+
+        return new Loan(
+                number,
+                "car",
+                400000,
+                3258,
+                18
+        );
+    }
+}
